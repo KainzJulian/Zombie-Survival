d57d0cac8f5b50d0c781b4bac6a0b8ca446ab7f3
JulianK123 <julian.kainz@htlpinkafeld.at>
2024-07-14 18:53:40 +0200

    refactor: changed path

--- Assets/ScriptableObject/NPCConfig.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NPCConfig", menuName = "Config/NPC")]
-public class NPCConfig : ScriptableObject
-{
-    public int movementSpeed;
-}
