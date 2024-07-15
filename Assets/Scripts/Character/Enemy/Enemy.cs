c67a64200b8f5c3cbec2a1a324d2474d2deab866
JulianK123 <julian.kainz@htlpinkafeld.at>
2024-07-15 10:07:22 +0200

    feat: changed zombie config

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -10,11 +10,14 @@ public class Enemy : MonoBehaviour
     [SerializeField] CircleCollider2D seekArea;
     [SerializeField] CharacterController2D characterController2D;
     [SerializeField] EnemyAI enemyAI;
+    [SerializeField] Health health;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyAI = GetComponent<EnemyAI>();
+
+        health.health = config.health;
 
         seekArea.radius = config.seekRadius;
         characterController2D.speed = config.movementSpeed;
--- Assets/Scripts/Character/Enemy/TestEnemyAI.cs
+++ Assets/Scripts/Character/Enemy/TestEnemyAI.cs
@@ -28,7 +28,6 @@ public class TestEnemyAI : MonoBehaviour, EnemyAI
 
     void Update()
     {
-        // meleeWeapon.attack(attackPoint, layer);
         if (!isAFK)
             return;
 
