b5771e877a937aa8457b5e5dc80e81afb5e39568
JulianK123 <julian.kainz@htlpinkafeld.at>
2024-07-18 20:40:00 +0200

    feat: finished noise system for walking

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -11,6 +11,8 @@ public class CharacterController2D : MonoBehaviour
 
     public float speed;
 
+    private float helpTime = 1;
+
     public enum SpeedType
     {
         SNEAK = 50,
@@ -26,9 +28,41 @@ public class CharacterController2D : MonoBehaviour
 
     public void moveFixed(float horizontal, float vertical, SpeedType type)
     {
+        baseMove(horizontal, vertical, type, Time.fixedDeltaTime);
+    }
+
+    private void baseMove(float horizontal, float vertical, SpeedType type, float deltaTime)
+    {
+        helpTime -= Time.deltaTime * getStepSpeed(type);
+
+        if (horizontal == 0 && vertical == 0)
+            return;
+
         float speedTypeMultiplier = (int)type / 100;
-        rb.MovePosition(rb.position + new Vector2(horizontal, vertical) * (speed * Time.fixedDeltaTime) * speedTypeMultiplier);
-        noiseSource.generateNoise(getNoiseRadius(type));
+        Vector2 posVector = new Vector2(horizontal, vertical);
+
+        rb.MovePosition(rb.position + posVector * speed * Time.fixedDeltaTime * speedTypeMultiplier);
+
+        if (helpTime <= 0)
+        {
+            noiseSource.generateNoise(getNoiseRadius(type));
+            helpTime = 1;
+        }
+    }
+
+    private float getStepSpeed(SpeedType type)
+    {
+        switch (type)
+        {
+            case SpeedType.SNEAK:
+                return 0.5f;
+            case SpeedType.WALK:
+                return 1f;
+            case SpeedType.SPRINT:
+                return 3f;
+        }
+
+        return 0;
     }
 
     private int getNoiseRadius(SpeedType type)
@@ -48,13 +82,18 @@ public class CharacterController2D : MonoBehaviour
 
     public void move(float horizontal, float vertical, SpeedType type)
     {
-        float speedTypeMultiplier = (int)type / 100;
-        rb.MovePosition(rb.position + speed * Time.deltaTime * new Vector2(horizontal, vertical) * speedTypeMultiplier);
+        baseMove(horizontal, vertical, type, Time.deltaTime);
+    }
+
+    public void moveFixed(Vector2 position, SpeedType type = SpeedType.WALK)
+    {
+        Vector2 newPosition = (new Vector3(position.x, position.y, 0) - transform.position).normalized;
+        baseMove(newPosition.x, newPosition.y, type, Time.deltaTime);
     }
 
     public void move(Vector2 position, SpeedType type = SpeedType.WALK)
     {
         Vector2 newPosition = (new Vector3(position.x, position.y, 0) - transform.position).normalized;
-        move(newPosition.x, newPosition.y, type);
+        baseMove(newPosition.x, newPosition.y, type, Time.deltaTime);
     }
 }
